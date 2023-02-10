6398da40fd16ddbead9b55d7b00bf32d7309fa16
Roshantiwary1 <tiwariroshan9999@gmail.com>
2023-02-10 14:42:31 +0530

    final

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': 'b3cd8a2395msh51377043a5dd809p18132ajsn6493200fccf4',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
